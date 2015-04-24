4ff9b3eef2d0b88711c95c2456c8964feea4f982
Rico Xie <ricoxie@users.noreply.github.com>
2015-04-25 02:38:18 +0800

    提交页面增加动态口令验证

--- src/main/java/com/bdclab/fetch/reference/Constant.java
+++ src/main/java/com/bdclab/fetch/reference/Constant.java
@@ -7,7 +7,8 @@ public class Constant {
 	public static String BDC_DAILY_TOKEN = getDailyToken();
 	
 	private static String getDailyToken() {
-		long now = (8 * 3600000 + (long) new Date().getTime()) / 100000;
+		// long now = (8 * 3600000 + (long) new Date().getTime()) / 100000;
+		long now = (long) new Date().getTime() / 100000 + 288;
 		long today = now - now % 864;
 		long check = ((today % 60649) * 167 + 524287) % 1000000;
 		return String.format("%06d", check);
